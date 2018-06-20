fd87f769c001350d0edaab5f957bf3d5346cf1f1
shuxinman <shuxinman@qq.com>
2018-06-20 11:41:13 +0800

    转换测试

--- word.php
+++ word.php
@@ -4,7 +4,7 @@ require 'vendor/autoload.php';
 $arr = array('doc', 'docx', 'xls', 'xlsx', 'ppt', 'pptx');
 
 set_time_limit(300);
-$converter = new \Word\PdfConverter();
+$converter = new \Word\WordToPdf();
 $source = __DIR__ . DIRECTORY_SEPARATOR . '1.doc';
 $export = __DIR__ . DIRECTORY_SEPARATOR . '2.pdf';
 echo $converter->execute($source, $export);
